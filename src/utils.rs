ec51915af71b9980593e9abe178d75a0172ec6bc
Eric Woolsey <ewoolsey@ualberta.ca>
2024-10-18 17:25:23 -0700

    remove ethers

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,6 @@
 use std::future::Future;
-use std::str::FromStr;
 use std::time::Duration;
 
-use ethers::types::H160;
-use serde::{de, Deserialize, Deserializer};
 use tracing::{error, warn};
 
 pub async fn retry<S, F, T, E>(
@@ -31,30 +28,5 @@ where
                 backoff *= 2;
             }
         }
-    }
-}
-
-pub fn deserialize_h160<'de, D>(deserializer: D) -> Result<H160, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    H160::from_str(&s).map_err(|e| {
-        de::Error::custom(format!("Failed to deserialize H160: {}", e))
-    })
-}
-
-pub fn deserialize_optional_h160<'de, D>(
-    deserializer: D,
-) -> Result<Option<H160>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s: Option<String> = Option::deserialize(deserializer)?;
-    match s {
-        Some(str_val) => H160::from_str(&str_val).map(Some).map_err(|e| {
-            de::Error::custom(format!("Failed to deserialize H160: {}", e))
-        }),
-        None => Ok(None),
     }
 }
