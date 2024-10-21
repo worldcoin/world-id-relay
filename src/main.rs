a9bf880c8d2943da9b2f3f3295267e143a26f549
0xOsiris <djosiris@proton.me>
2024-10-21 13:51:47 -0700

    change signer variants to an enum

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use clap::Parser;
 use config::{NetworkType, WalletConfig};
 use eyre::eyre::Result;
 use futures::StreamExt;
-use relay::signer::{AlloySigner, RelaySigner};
+use relay::signer::{AlloySigner, Signer};
 use relay::{EVMRelay, Relayer};
 use telemetry_batteries::metrics::statsd::StatsdBattery;
 use telemetry_batteries::tracing::datadog::DatadogBattery;
@@ -126,9 +126,7 @@ pub async fn run(config: Config) -> Result<()> {
     Ok(())
 }
 
-fn init_relays(
-    cfg: Config,
-) -> Result<Vec<Relayer<impl RelaySigner, impl RelaySigner>>> {
+fn init_relays(cfg: Config) -> Result<Vec<Relayer>> {
     let mut relayers = Vec::new();
     cfg.bridged_networks.iter().for_each(|n| match n.ty {
         NetworkType::Evm => {
@@ -154,13 +152,11 @@ fn init_relays(
 
                     let signer = AlloySigner::new(state_bridge);
 
-                    relayers.push(Relayer::<_, AlloySigner>::Evm(
-                        EVMRelay::new(
-                            signer,
-                            n.world_id_address,
-                            n.provider.rpc_endpoint.clone(),
-                        ),
-                    ));
+                    relayers.push(Relayer::Evm(EVMRelay::new(
+                        Signer::Alloy(signer),
+                        n.world_id_address,
+                        n.provider.rpc_endpoint.clone(),
+                    )));
                 }
                 _ => unimplemented!(),
             };
