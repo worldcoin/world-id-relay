1efb8173b50d6aca5cb7eb6d2b3a3c03b3dc8531
0xOsiris <djosiris@proton.me>
2024-10-21 13:51:47 -0700

    change signer variants to an enum

--- src/relay/signer.rs
+++ src/relay/signer.rs
@@ -1,18 +1,36 @@
-use alloy::{
-    network::{Ethereum, EthereumWallet},
-    providers::{
-        fillers::{
-            BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill,
-            NonceFiller, WalletFiller,
-        },
-        RootProvider,
-    },
-    transports::http::Http,
+use alloy::network::{Ethereum, EthereumWallet};
+use alloy::providers::fillers::{
+    BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill,
+    NonceFiller, WalletFiller,
 };
+use alloy::providers::RootProvider;
+use alloy::transports::http::Http;
 use eyre::eyre::Result;
 use tracing::{error, info};
 
 use crate::abi::IStateBridge::IStateBridgeInstance;
+
+pub trait RelaySigner {
+    #[allow(async_fn_in_trait)]
+    /// Propogate a new Root to the State Bridge for the given network.
+    async fn propagate_root(&self) -> Result<()>;
+}
+
+#[derive(Debug, Clone)]
+pub enum Signer {
+    Alloy(AlloySigner),
+    TxSitter, // TODO: Implement this
+}
+
+impl RelaySigner for Signer {
+    async fn propagate_root(&self) -> Result<()> {
+        match self {
+            Signer::Alloy(signer) => signer.propagate_root().await,
+            Signer::TxSitter => unimplemented!(),
+        }
+    }
+}
+
 pub type AlloySignerProvider = FillProvider<
     JoinFill<
         JoinFill<
@@ -28,11 +46,8 @@ pub type AlloySignerProvider = FillProvider<
     Http<reqwest::Client>,
     Ethereum,
 >;
-pub trait RelaySigner {
-    /// Propogate a new Root to the State Bridge for the given network.
-    async fn propagate_root(&self) -> Result<()>;
-}
 
+#[derive(Debug, Clone)]
 pub struct AlloySigner {
     pub(crate) state_bridge_instance:
         IStateBridgeInstance<Http<reqwest::Client>, AlloySignerProvider>,
